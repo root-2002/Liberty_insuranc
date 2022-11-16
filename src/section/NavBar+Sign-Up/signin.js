631c6e9909c79c1cfccf4689fd8dbb1404ad397a
AboodWJ <s12114616@stu.najah.edu>
2022-11-16 22:58:52 +0200

    Got back to an Old commit

--- src/section/NavBar+Sign-Up/signin.js
+++
@@ -1,6 +0,0 @@
-const container = document.querySelector(".container");
-const signUpBtn = document.querySelector(".green-bg button");
-
-signUpBtn.addEventListener("click", () => {
-  container.classList.toggle("change");
-});
