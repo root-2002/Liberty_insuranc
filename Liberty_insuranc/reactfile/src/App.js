27965f4512fca13277e24750ca9b5584d68386e0
AboodWJ <s12114616@stu.najah.edu>
2022-11-23 20:59:22 +0200

    HomePage Not Complete
    Signed-off-by: AboodWJ <s12114616@stu.najah.edu>

--- Liberty_insuranc/reactfile/src/App.js
+++ Liberty_insuranc/reactfile/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import HomePage from './Header/HomePage/HomePage'
 
 function App() {
   return (
     <div className="App">
-    
+    <HomePage />
     </div>
   );
 }
---
+++ Liberty_insuranc/reactfile/src/Header/HomePage/HomePage.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './HomePage.css'
+
+export default function HomePage() {
+  return (
+    
+        <div>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossOrigin="anonymous" />
+  
+  <section>
+    <header>
+      <div className="container">
+        <div className="banner-text">
+          <div className="text-area">
+            <h2>Welcome To Liberty Insurance</h2>
+          </div>
+          <p>"An ounce of prevention is better than a pound of cure"</p>
+          <br />
+          <p>At Liberty Insurance, we specialize in life insurance plans that provide peace of mind for your family.</p><br /><p>
+            Over the years, we have observed how complicated it has become to get protection. That's why we do all the heavy paperwork for you and simplify the whole process.</p><br /><p>
+            All you have to do is have a friendly, pressure-free chat with our experienced licensed advisors from the comfort of your home.</p>
+        </div>
+      </div>
+    </header>
+  </section>
+
+
+      
+    </div>
+  )
+}
