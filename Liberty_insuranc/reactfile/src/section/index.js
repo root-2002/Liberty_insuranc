646110e088dd7ac19aefa558bc4ec564ec52d637
AboodWJ <s12114616@stu.najah.edu>
2022-11-25 18:16:51 +0200

    changed the places of foliders to merge with ease

--- Liberty_insuranc/reactfile/src/section/index.js
+++
@@ -1,3 +0,0 @@
-export {default as Header} from './Header/Header';
-export {default as InsuranceDepartments} from './InsuranceDepartments/InsuranceDepartments';
-export {default as Footer} from './Footer/Footer';
