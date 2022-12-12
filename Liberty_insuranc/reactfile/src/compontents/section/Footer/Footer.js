1d6928c46daf6fb3295b1bd97f167e0d4ced2ecf
Ahmad <mansourahmad305@gmail.com>
2022-12-12 15:15:56 +0200

    footer update

--- Liberty_insuranc/reactfile/src/compontents/section/Footer/Footer.js
+++ Liberty_insuranc/reactfile/src/compontents/section/Footer/Footer.js
@@ -1,41 +1,56 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {FaHome,FaPhoneAlt,FaEnvelope,FaPrint,FaFacebook,FaInstagram} from 'react-icons/fa'
-import "./footer.css"
+import "./Footer.css"
+import { Link } from 'react-router-dom';
 function footer(){
   return (
     <div className='bg-dark'>
     <div className='container-footer'>
         <div className='row py-5 text-white'>
-          <div className='col-md-3 col-sm-12'>
-            <h4 className="text-danger">liberty insurance</h4><br/>
-            <p>
+          <div className='col-lg-3 col-sm-12'>
+            <h4 className="text-danger footerText">liberty insurance</h4>
+            <p className='footerText'>
             Yes, state laws set the minimum for what type and amount of coverage people need for auto insurance,
              but that doesn't mean it's enough for you. We're here to help you find the right coverages for you. 
              That's why we've made it easy to customize your car insurance.
             </p><br/>
           </div>
-          <div className='col-md-3 col-sm-12'>
-            <h4 className="text-danger">services</h4>
-            <p>about us</p>
-            <p>Know more</p>
-            <p>customer serv</p>
+          <div className='col-lg-3 col-sm-12'>
+            <h4 className="text-danger footerText">services</h4>
+            <p className='footerText'>about us</p>
+            <p className='footerText'>Know more</p>
+            <p className='footerText'>customer serv</p>
           </div>
-          <div className='col-md-3 col-sm-12'>
+          <div className='col-lg-3 col-sm-12'>
             <h4 className="text-danger">our Insurances </h4>
-            <p>Car Insurances</p>
-            <p>Life Insurance</p>
-            <p> Health Insurance</p>
-            <p>Home Insurance</p>
+            <Link to='/car'>
+            <p className='footerText'>Car Insurances</p>
+            </Link>
+            <Link to='/Life'>
+            <p className='footerText'>Life Insurance</p>
+            </Link>
+            <Link to='/Health'>
+            <p className='footerText'> Health Insurance</p>
+            </Link>
+            <Link to='/Home'>
+            <p className='footerText'>Home Insurance</p>
+            </Link>
+            <Link to='/Bank'>
+            <p className='footerText'>Bank Insurance</p>
+            </Link>
+            <Link to='/Contracts'>
+            <p className='footerText'>Contracts Insurance</p>
+            </Link>
           </div>
-          <div className='col-md-3 col-sm-12'>
+          <div className='col-lg-3 col-sm-12 footerLinks'>
             <h4 className="text-danger">Address</h4>
-            <FaHome /><a href="https://goo.gl/maps/aLrx65LYoaqjpLUa6"target="_blank">  Nablus </a><br />
-            <FaPhoneAlt/> <a href="+972 55456489">+972 55456489</a><br />
-            <FaEnvelope /><a herf="liberty9999@gmail.com">Liberty9999@gmail.com</a><br />
-            <FaPrint /><a herf="+09 5456564">+09 54564564</a><br />
-            <FaFacebook/><a herf="liberty_insurance"></a>Liberty_insurance<br />
-            <FaInstagram/>Liberty_insurance999<br />
+            <FaHome /><a href="https://goo.gl/maps/aLrx65LYoaqjpLUa6"target="_blank" className='footerText'>  Nablus </a><br />
+            <FaPhoneAlt/> <a href='https://github.com/root-2002/Liberty_insuranc' className='footerText' >+972 55456489</a><br />
+            <FaEnvelope /><a href='https://github.com/root-2002/Liberty_insuranc' className='footerText'>Liberty9999@gmail.com</a><br />
+            <FaPrint /><a href='https://github.com/root-2002/Liberty_insuranc' className='footerText'>+09 54564564</a><br />
+            <FaFacebook/><a href='https://github.com/root-2002/Liberty_insuranc' className='footerText'>Liberty_insurance</a><br />
+            <FaInstagram/><a className='footerText' href='https://github.com/root-2002/Liberty_insuranc'>Liberty_insurance999</a><br />
                </div>
              </div>     
             <div className='text-center text-white p-4'>
@@ -48,4 +63,4 @@ function footer(){
           </div>
           )
   }
-  export default footer 
+  export default footer
