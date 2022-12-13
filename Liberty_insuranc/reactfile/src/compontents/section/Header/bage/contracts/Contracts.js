8937268d6035a4a1db035a819fe35eb47f8ac234
AboodWJ <s12114616@stu.najah.edu>
2022-12-13 16:46:01 +0200

    added contarct page

--- Liberty_insuranc/reactfile/src/compontents/section/Header/bage/contracts/Contracts.js
+++ Liberty_insuranc/reactfile/src/compontents/section/Header/bage/contracts/Contracts.js
@@ -7,43 +7,25 @@ function contracts() {
 
       <div className='bg-Info'>
         <div className='divStyle'>
-          <h1 className='pageHeader'>Home Insurance</h1>
-          <p className='pageHeaderpara'>Protect The Place Where Memories Are Made </p>
+          <h1 className='pageHeader'>Insurance Contracts</h1>
         </div>
 
       </div>
     </div>
     <div className='bgBackBone'></div>
     <div className='container insInfo'>
-      <h1 className='insText'>What is home insurance?</h1>
-      <p className='insText ins-Para'>Home insurance covers loss and damage to your house and household contents. For example, if your home is damaged by a flood or fire, your insurance provider could cover the cost of any repairs or replacements needed.
-
-        There are two types of home cover: buildings insurance and contents insurance. You can buy them individually or together as a combined policy.</p>
+      <h1 className='insText'>How to Easily Understand Your Insurance Contract</h1>
+      <p className='insText ins-Para'>There are certain types of insurance most people need to have. For example, if you own a home then homeowner's insurance may be standard. Auto insurance covers your vehicle while life insurance protects you and your loved ones in a worst-case scenario.
+      <br/><br/>
+      When your insurer gives you the policy document, it's important to read through it carefully to make sure you understand it. Your insurance advisor is always there for you to help you with the tricky terms in the insurance forms, but you should also know for yourself what your contract says. In this article, we'll make reading your insurance contract easy, so you understand their basic principles and how they are put to use in daily life.</p>
       <div className='container d-md-flex'>
         <div class="card ins-CardsPage">
           <div class="card-body">
-            <h5 class="card-title">Buildings insurance</h5>
-            <p class="card-text insCardPara">Buildings insurance covers the structure of the property and its permanent fixtures and fittings. It may also cover driveways, patios, walls, fences, and any outbuildings including garages.<br /><br />
-
-              Most mortgage providers expect you to have it. If you're renting you don't need it.</p>
-          </div>
-        </div>
-        <div class="card ins-CardsPage">
-          <div class="card-body">
-            <h5 class="card-title">Contents insurance</h5>
-            <p class="card-text insCardPara">Contents insurance covers damage or theft of your household items (e.g. your home appliances, furniture and jewellery).<br /><br />
-
-              Each item is only covered up to a limited amount. You'll see this called a 'single item limit', and it's usually around $1,500 but could be higher or lower.<br /><br />
-
-              Accidental damage and damage to items outside the home aren't covered as standard but can be for an extra cost.</p>
-          </div>
-        </div>
-        <div class="card ins-CardsPage">
-          <div class="card-body">
-            <h5 class="card-title">Combined home insurance</h5>
-            <p class="card-text insCardPara">A combined buildings and contents insurance policy covers the structure of your home and your belongings.<br /><br />
-
-              If you need both, then taking out one policy with the same provider usually works out cheaper and can make things simpler if you need to make a claim (e.g. if there’s a fire it’ll be far easier reporting it all to one insurer instead of two).</p>
+            <h5 class="card-title">Key TakeAways</h5>
+            <p class="card-text insCardPara"><br/>• Life insurance contracts spell out the terms of your policy, including what's covered and what's not as well as what you'll pay.<br/><br/>
+• A life insurance contract can contain terminology and jargon that you may not be immediately familiar with.<br/><br/>
+• It's important to read through an insurance contract carefully before signing so you understand what you're agreeing to.<br/><br/>
+• You should also review the contract to check for any errors that may affect your coverage or costs.<br/><br/></p>
           </div>
         </div>
 
@@ -52,37 +34,45 @@ function contracts() {
     </div>
     <br /><br />
     <div className='imgTextIns'>
-      <h1 className='insText'>How much is home insurance?</h1>
+      <h1 className='insText'>Insurance Contract Essentials</h1>
       <div className='container d-lg-flex'>
-        <p>The average price paid by GoCompare customers for a combined home insurance policy is $191 a year.<br />
+        <p><br/>When reviewing an insurance contract, there are certain things included that are typically universal.<br/><br/>
 
-          Buildings-only or contents-only cover is cheaper than a combined policy, but you won't get the same level of cover.<br />
-
-          According to our data, the average price paid for home insurance per year by cover type is:<br /><br />
-
-          1-Buildings and contents insurance - $191<br />
-          2-Buildings only insurance - $150<br />
-          3-Contents only insurance - $75<br /><br />
-          Things like the location, size of your house and previous claims can all impact the cost of your premium.<br /><br />
-          *The average price paid for home insurance purchased in July 2022 by type of cover. For buildings and contents insurance, it was $191. For buildings insurance only, it was $150. For contents insurance only, it was $75.
-
+• Offer and Acceptance. When applying for insurance, the first thing you do is get the proposal form of a particular insurance company. After filling in the requested details, you send the form to the company (sometimes with a premium check). This is your offer. If the insurance company agrees to insure you, this is called acceptance. In some cases, your insurer may agree to accept your offer after making some changes to your proposed terms.<br/><br/>
+• Consideration. This is the premium or the future premiums that you have to pay to your insurance company. For insurers, consideration also refers to the money paid out to you should you file an insurance claim. This means that each party to the contract must provide some value to the relationship.<br/><br/>
+• Legal Capacity. You need to be legally competent to enter into an agreement with your insurer. If you are a minor or are mentally ill, for example, then you may not be qualified to make contracts. Similarly, insurers are considered to be competent if they are licensed under the prevailing regulations that govern them.<br/><br/>
+• Legal Purpose. If the purpose of your contract is to encourage illegal activities, it is invalid.<br/>
         </p>
-        <img src='./imgs/home.png' className='insTextImg-Img'alt =""></img>
       </div>
     </div>
     <div className='moreInfo'>
       <div className='container'>
-        <h1 className='moreInfoh1'>How can I get cheaper home insurance?</h1>
-        <p className='moreInfoP'><br />
-          Here's eight tips that could help keep the cost of your premium down:<br /><br />1-Shop around for quotes - In the last year, 77% of people didn’t switch their home insurance to a new provider, potentially missing out on cheaper premiums.<br /><br />
-          2-Buy 29 days before your policy ends - Our research shows it’ll cost around 16% less than it would to buy it the day before your existing policy renews.<br /><br />
-          3-Improve your home security - Get insurer-approved locks for your home, as well as smoke and burglar alarm systems.<br /><br />
-          4-Regularly maintain your home - Staying on top of things and getting repairs done early can save you money in the long run.<br /><br />
-          5-Build your no-claim discount - For every year you don't make a claim, you'll likely reduce the cost of your premium.<br /><br />
-          6-Don't over-insure - Overestimating the value of your contents and the rebuild cost of your home will increase your premium. But if you're in any doubt, it's better to overestimate than underestimate.<br /><br />
-          7-Only pay for what you need - You'll be offered a variety of optional extras when you apply for home insurance. Remember the best home insurance policy for you is one that offers the cover you need at an affordable price.<br /><br />
-          8-Pay upfront - With monthly payments, you'll have to pay interest and sometimes a finance arrangement fee, so it'll be more expensive.<br /><br />
+        <p className='moreInfoP'><br /><h1>Contract Values</h1><br/>
+This section of an insurance contract specifies what the insurance company may pay out to you for an eligible claim, as well as what you may pay to the insurer for a deductible. How these sections of an insurance contract are structured often depends on whether you have an indemnity or non-indemnity policy.<br/>
 
+<h1>Indemnity Contracts</h1><br/>
+Most insurance contracts are indemnity contracts. Indemnity contracts apply to insurances where the loss suffered can be measured in terms of money.<br/><br/>
+
+Principle of Indemnity. This states that insurers pay no more than the actual loss suffered. The purpose of an insurance contract is to leave you in the same financial position you were in immediately prior to the incident leading to an insurance claim. When your old Chevy Cavalier is stolen, you can't expect your insurer to replace it with a brand new Mercedes-Benz. In other words, you will be remunerated according to the total sum you have assured for the car.<br/>
+(To read more on indemnity contracts, see "Shopping for Car Insurance" and "How Does the 80% Rule for Home Insurance Work?")<br/><br/>
+
+There are some additional factors of your insurance contract that create situations in which the full value of an insured asset is not remunerated.<br/><br/>
+
+Under-Insurance. Often, in order to save on premiums, you may insure your house at $80,000 when the total value of the house actually comes to $100,000. At the time of partial loss, your insurer will pay only a proportion of $80,000 while you have to dig into your savings to cover the remaining portion of the loss. This is called under-insurance, and you should try to avoid it as much as possible.<br/><br/>
+Excess. To avoid trivial claims, the insurers have introduced provisions like excess. For example, you have auto insurance with the applicable excess of $5,000. Unfortunately, your car had an accident with the loss amounting to $7,000. Your insurer will pay you the $7,000 because the loss has exceeded the specified limit of $5,000. But, if the loss comes to $3,000 then the insurance company will not pay a single penny and you have to bear the loss expenses yourself. In short, the insurers will not entertain claims unless and until your losses exceed a minimum amount set by the insurer.<br/><br/>
+Deductible. This is the amount you pay in out-of-pocket expenses before your insurer covers the remaining expense. Therefore, if the deductible is $5,000 and the total insured loss comes to $15,000, your insurance company will only pay $10,000. The higher the deductible, the lower the premium and vice versa.<br/><br/>
+<h1>Non-Indemnity Contracts</h1><br/>
+Life insurance contracts and most personal accident insurance contracts are non-indemnity contracts. You may purchase a life insurance policy of $1 million, but that does not imply that your life's value is equal to this dollar amount. Because you can't calculate your life's net worth and fix a price on it, an indemnity contract does not apply.<br/><br/>
+
+A life insurance contract typically includes the following:<br/><br/>
+
+Declarations page: This is often the first page of a life insurance policy and it includes the policy owner's name, the policy type and number, issue date, effective date, premium class or rate class and any riders you've chosen to add on. If you purchased a term life policy, the declarations page should also specify the length of the coverage term.<br/><br/>
+Policy terms and definitions: You may see a separate section in your life insurance contract that breaks down terms and definitions, including death benefit, premium, beneficiary and insurance age. Your insurance age may be your actual age or the nearest age assigned to you by the life insurance company.<br/><br/>
+Coverage details: The coverage details section of a life insurance contract provides in-depth information about your policy, including how much you'll pay for premiums, when those payments are due, penalties for missing payments and who your policy's death benefits should be paid out to. For example, you may have just one primary beneficiary or a primary beneficiary with several contingent beneficiaries.<br/><br/>
+Additional policy details: There may be a separate section in your life insurance contract that covers riders if you've chosen to add any on. Riders expand your policy's coverage. Common life insurance riders include accelerated death benefit riders, long-term care riders and critical illness riders. These add-ons allow you to tap into your death benefit while still living if you need money to cover expenses related to a terminal illness.<br/><br/>
+When you've determined that life insurance is something you need, it's important to compare the options carefully. For example, you may lean toward term life insurance versus permanent life insurance if you don't need lifetime coverage. Or you may prefer permanent coverage if you're treating life insurance like an investment.<br/><br/>
+
+In either scenario, it's important to shop around to find the best life insurance companies.<br/>
         </p>
       </div>
     </div>
